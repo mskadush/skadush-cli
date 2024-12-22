4e0f872c1f88990d8b1711e597bec001e87d5889
Minenhle Sithole <minenhle@skadush.co.za>
2024-12-22 16:38:53 +0200

    Compile directly to application

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,12 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-
 plugins {
     kotlin("jvm")
-    id("org.jetbrains.compose")
-    id("org.jetbrains.kotlin.plugin.compose")
+    application
 }
 
 group = "za.co.skadush.cli"
 version = "1.0-SNAPSHOT"
 
 dependencies {
-    // Note, if you develop a library, you should use compose.desktop.common.
-    // compose.desktop.currentOs should be used in launcher-sourceSet
-    // (in a separate module for demo project and in testMain).
-    // With compose.desktop.common you will also lose @Preview functionality
-    implementation(compose.desktop.currentOs)
 
     implementation("com.github.ajalt.clikt:clikt:5.0.2")
 
@@ -22,14 +14,6 @@ dependencies {
     implementation("com.github.ajalt.clikt:clikt-markdown:5.0.2")
 }
 
-compose.desktop {
-    application {
-        mainClass = "MainKt"
-
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "cli"
-            packageVersion = "1.0.0"
-        }
-    }
+application {
+    mainClass.set("za.co.skadush.cli.MainKt")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,6 @@ pluginManagement {
 
     plugins {
         kotlin("jvm").version(extra["kotlin.version"] as String)
-        id("org.jetbrains.compose").version(extra["compose.version"] as String)
-        id("org.jetbrains.kotlin.plugin.compose").version(extra["kotlin.version"] as String)
     }
 }
 
