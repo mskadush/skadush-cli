934f406a29c350175937eaa886b6e9db82ffdad7
Minenhle Sithole <minenhlesithole@Minenhles-MacBook-Pro.local>
2024-12-22 15:27:07 +0200

    init working stuff

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.compose")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+group = "za.co.skadush.cli"
+version = "1.0-SNAPSHOT"
+
+dependencies {
+    // Note, if you develop a library, you should use compose.desktop.common.
+    // compose.desktop.currentOs should be used in launcher-sourceSet
+    // (in a separate module for demo project and in testMain).
+    // With compose.desktop.common you will also lose @Preview functionality
+    implementation(compose.desktop.currentOs)
+
+    implementation("com.github.ajalt.clikt:clikt:5.0.2")
+
+    // optional support for rendering markdown in help messages
+    implementation("com.github.ajalt.clikt:clikt-markdown:5.0.2")
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "cli"
+            packageVersion = "1.0.0"
+        }
+    }
+}
