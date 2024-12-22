e6707b57a94800ef7e96e7a0559b4d33ed150ea9
Minenhle Sithole <minenhle@skadush.co.za>
2024-12-22 18:57:18 +0200

    Tryna publish 2

--- src/main/kotlin/za/co/skadush/cli/Main.kt
+++ src/main/kotlin/za/co/skadush/cli/Main.kt
@@ -9,27 +9,29 @@ import com.github.ajalt.clikt.parameters.options.prompt
 import com.github.ajalt.clikt.parameters.types.int
 
 class Hello : CliktCommand() {
-    private val count: Int by option().int().default(1).help("Number of greetings")
-    private val name: String by option().prompt("Your name").help("The person to greet")
-    private val gs: Array<String> = "git status".split(" ").toTypedArray()
+//    private val count: Int by option().int().default(1).help("Number of greetings")
+    private val version: String = "1.x.x"
+//    private val name: String by option().prompt("Your name").help("The person to greet")
+//    private val gs: Array<String> = "git status".split(" ").toTypedArray()
 
     override fun run() {
-        repeat(count) {
-            echo("Hello $name!")
-        }
-        try {
-            // Execute the command
-            val process = ProcessBuilder(*gs)
-                .redirectOutput(ProcessBuilder.Redirect.INHERIT)
-                .redirectError(ProcessBuilder.Redirect.INHERIT)
-                .start()
-
-            // Wait for the process to finish and capture the exit code
-            val exitCode = process.waitFor()
-            echo("Command finished with exit code $exitCode")
-        } catch (e: Exception) {
-            echo("Failed to execute command: ${e.message}")
-        }
+//        repeat(count) {
+//            echo("Hello $count!")
+//        }
+        echo("version: $version")
+//        try {
+//            // Execute the command
+//            val process = ProcessBuilder(*gs)
+//                .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+//                .redirectError(ProcessBuilder.Redirect.INHERIT)
+//                .start()
+//
+//            // Wait for the process to finish and capture the exit code
+//            val exitCode = process.waitFor()
+//            echo("Command finished with exit code $exitCode")
+//        } catch (e: Exception) {
+//            echo("Failed to execute command: ${e.message}")
+//        }
     }
 }
 
